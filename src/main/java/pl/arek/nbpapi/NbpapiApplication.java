c5191027a3957427f2819d305ece3e6f129e8108
Arkadiusz Stankiewicz <sughalith@gmail.com>
2018-10-27 14:52:25 +0200

    initial commit

---
+++ src/main/java/pl/arek/nbpapi/NbpapiApplication.java
@@ -0,0 +1,12 @@
+package pl.arek.nbpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbpapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbpapiApplication.class, args);
+	}
+}
